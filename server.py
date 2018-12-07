5c7ad84e576ca59c100dd841a26417149d108af1
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-12-07 21:53:58 +0300

    Added HTTP basic access authentication for admin interface

--- server.py
+++ server.py
@@ -1,19 +1,37 @@
 import os
 
 from flask import Flask
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
+from flask_basicauth import BasicAuth
 
 from db import PizzaType, PizzaChoice, db_session
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
 
-admin = Admin(app, name='Pizza Shop', template_mode='bootstrap3')
+basic_auth = BasicAuth(app)
+
+
+class PizzaAdminIndexView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        if not basic_auth.authenticate():
+            return basic_auth.challenge()
+
+        return super(PizzaAdminIndexView, self).index()
 
 
 class PizzaBaseModelView(ModelView):
     can_set_page_size = True
+
+    def is_accessible(self):
+        if not basic_auth.authenticate():
+            return False
+
+        return True
 
 
 class PizzaTypeModelView(PizzaBaseModelView):
@@ -27,6 +45,12 @@ class PizzaChoiceModelView(PizzaBaseModelView):
     column_labels = dict(pizza_type='Вид пиццы', title='Формат', price='Цена')
 
 
+admin = Admin(
+    app,
+    name='Пиццерия',
+    template_mode='bootstrap3',
+    index_view=PizzaAdminIndexView(name='Главная'),
+)
 admin.add_view(PizzaTypeModelView(PizzaType, db_session, 'Виды пиццы'))
 admin.add_view(PizzaChoiceModelView(PizzaChoice, db_session, 'Ассортимент'))
 
