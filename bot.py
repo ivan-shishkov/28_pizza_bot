d6aa343b8c6434f5da03b1b4246be9f31c96e6f3
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-12-08 19:43:31 +0300

    Added load of pizzas catalog from database

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from jinja2 import Template
 from os import getenv
 
-from models import catalog
+from db import db_session, PizzaType
 
 TOKEN = getenv('BOT_TOKEN')
 if not TOKEN:
@@ -11,18 +11,26 @@ if not TOKEN:
 bot = telebot.TeleBot(TOKEN)
 
 with open('templates/catalog.md', 'r') as catalog_file:
-    catalog_tmpl = Template(catalog_file.read())
+    catalog_template = Template(catalog_file.read())
 
 with open('templates/greetings.md', 'r') as greetings_file:
-    greetings_tmpl = Template(greetings_file.read())
+    greetings_template = Template(greetings_file.read())
+
 
 @bot.message_handler(commands=['start'])
 def greet(message):
-    bot.send_message(message.chat.id, greetings_tmpl.render())
+    bot.send_message(message.chat.id, greetings_template.render())
+
 
 @bot.message_handler(commands=['menu'])
 def show_catalog(message):
-    bot.send_message(message.chat.id, catalog_tmpl.render(catalog=catalog), parse_mode='Markdown')
+    catalog = db_session.query(PizzaType).all()
+    bot.send_message(
+        message.chat.id,
+        catalog_template.render(catalog=catalog),
+        parse_mode='Markdown',
+    )
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
